70217469a28ab79f700c8f2d40d8f3c9374ed4ae
Truc Truc <ntttruc@apcs.vn>
2019-08-29 14:49:10 +0700

    add new module

---
+++ database/data.go
@@ -0,0 +1,26 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
--- handler/username_controller.go
+++ handler/username_controller.go
@@ -1,7 +1,28 @@
 package handler
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func Print() {
-	fmt.Println("Hello World!")
+func Get(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET")
+}
+
+func Gett(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET")
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST")
+}
+func Put(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PUT")
+}
+
+func Patch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATCH")
+}
+func Delete(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("DELETE")
 }
