c2b6f57bbae6fd831e1d3a8d4515108af4c263ce
Truc Truc <ntttruc@apcs.vn>
2019-08-29 14:47:19 +0700

    add new module

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/trucntt1101/testusername/handler"
-
 	"github.com/go-chi/chi"
+	"github.com/trucntt1101/testusername/database"
+	"github.com/trucntt1101/testusername/handler"
 )
 
 func main() {
+
+	database.ConnectDB
 	r := chi.NewRouter()
 
-	r.Get("/", handler.Print())
+	r.Get("/users", handler.Get)
+	r.Get("/users/{id}", handler.Gett)
+	r.Post("/users", handler.Post)
+	r.Put("/users", handler.Put)
+	r.Patch("/users", handler.Patch)
+	r.Delete("/users", handler.Delete)
+
 	http.ListenAndServe(":3333", r)
 }
